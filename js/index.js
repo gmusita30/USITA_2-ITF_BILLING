6e2ccd1b64949dee436795ff16ef0fc57c1d0c75
gmusita30 <gabrieladrian.usita.iics@ust.edu.ph>
2020-02-13 20:57:15 +0800

    second commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 /*Add the JavaScript here for the function function billingFunction().  It is responsible for setting and clearing the fields in Billing Information */
 function billingFunction(){
     
-    if (document.getElementById('same').checked){
-        var shippingName = document.getElementById('shippingName').value;
-        var shippingZip = document.getElementById('shippingZip').value;
-        
-        document.getElementById('billingName').value = shippingName;
-        document.getElementById('billingZip').value = shippingZip;
+    if (document.getElementById('same').checked)
+    {
+        document.getElementById('billingName').value = document.getElementById('shippingName').value;
+        document.getElementById('billingZip').value = document.getElementById('shippingZip').value;
  
     }
-    else{ 
+    else
+    { 
         document.getElementById('billingName').value = "";
         document.getElementById('billingZip').value = "";
     }
